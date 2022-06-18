bca157c226bab78388c86c744d79b086ef92711d
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2022-06-19 00:39:18 +0200

    Weitere Implementierung des SrtLoader.

--- Readers/ISubtitlesLoader.cs
+++ Readers/ISubtitlesLoader.cs
@@ -1,8 +1,17 @@
 ﻿
 namespace Readers
 {
+    /// <summary>
+    /// Interface for loading subtitles.
+    /// </summary>
     public interface ISubtitlesLoader
     {
-        public IList<Subtitle> LoadSubtitlesFromFile(string filePath);
+        /// <summary>
+        /// Load subtitles from a file.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        /// <param name="countOfChars">Receives the count of characters of all captions.</param>
+        /// <returns>A list with all subtitles.</returns>
+        public IList<Subtitle> LoadSubtitlesFromFile(string filePath, out int countOfChars);
     }
 }
--- Readers/SrtLoader.cs
+++ Readers/SrtLoader.cs
@@ -11,19 +11,30 @@ namespace Readers
         private static readonly Regex _subtitleRegex =
             new (@"\d+\r\n(?<start>\d{2}:\d{2}:\d{2},\d{3}) --> (?<end>\d{2}:\d{2}:\d{2},\d{3})\r\n(?<caption>.+(?:\r\n.+)*)\r\n", RegexOptions.Compiled);
 
-        public IList<Subtitle> LoadSubtitlesFromFile(string filePath)
+        /// <inheritdoc/>
+        public IList<Subtitle> LoadSubtitlesFromFile(string filePath, out int countOfChars)
         {
+            CultureInfo culture = new("de-DE");
+            countOfChars = 0;
+
             string text = File.ReadAllText(filePath);
             MatchCollection matches = _subtitleRegex.Matches(text);
+            List<Subtitle> allSubtitles = new(capacity: matches.Count);
 
-            CultureInfo culture = new("de-DE");
-            return (from match in matches
-                    select new Subtitle
-                    {
-                        caption = match.Groups["caption"].Value,
-                        startTime = TimeSpan.Parse(match.Groups["start"].Value, culture),
-                        endTime = TimeSpan.Parse(match.Groups["end"].Value, culture)
-                    }).ToArray();
+            foreach (Match match in matches)
+            {
+                Subtitle subtitle = new()
+                {
+                    caption = match.Groups["caption"].Value,
+                    startTime = TimeSpan.Parse(match.Groups["start"].Value, culture),
+                    endTime = TimeSpan.Parse(match.Groups["end"].Value, culture)
+                };
+
+                allSubtitles.Add(subtitle);
+                countOfChars += subtitle.caption.Length;
+            }
+
+            return allSubtitles;
         }
     }
 }
